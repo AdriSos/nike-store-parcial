5862972ad993bfd53c7f9f849556d053333d9ebc
Adriana <adri.gonz.2004s@gmail.com>
2025-11-30 22:32:25 -0600

    base de datos

--- db.js
+++ db.js
@@ -1,20 +1,19 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-// Ajusta usuario y password según tu configuración de XAMPP
-mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error('Error conectando a la BD:', err);
-        return;
-    }
-    console.log('Conectado a MySQL exitosamente');
+  if (err) {
+    console.error("Error conectando a MySQL:", err);
+    return;
+  }
+  console.log("Conectado a MySQL en Railway");
 });
 
 module.exports = connection;
