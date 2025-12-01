875a23785d6f15ba8d13e65b668a100ffacd5093
Adriana <adri.gonz.2004s@gmail.com>
2025-11-30 23:12:58 -0600

    base

--- db.js
+++ db.js
@@ -1,13 +1,25 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
+// Debug: revisar variables de entorno
+console.log("Valores de conexión a MySQL:");
+console.log({
+  host: process.env.MYSQL_URL ? process.env.MYSQL_URL.split("@")[1].split("/")[0].split(":")[0] : "mysql.railway.internal",
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQLPORT
 });
 
+// Crear conexión
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_URL ? process.env.MYSQL_URL.split("@")[1].split("/")[0].split(":")[0] : "mysql.railway.internal",
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQLPORT ? Number(process.env.MYSQLPORT) : 3306
+});
+
+// Intentar conectar
 connection.connect((err) => {
   if (err) {
     console.error("Error conectando a MySQL:", err);
